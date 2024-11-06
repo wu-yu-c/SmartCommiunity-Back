a3768537faca9a1c562301a465fdf2ede612e337
wu-yu-c <2771381914@qq.com>
2024-11-06 10:09:24 +0800

    后端登录注册接口实现

--- src/main/java/com/example/SmartCommunity/controller/UserController.java
+++ src/main/java/com/example/SmartCommunity/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.SmartCommunity.controller;
 
 import com.example.SmartCommunity.service.UserService;
 import com.example.SmartCommunity.model.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +16,18 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "注册接口")
     @PostMapping("/register")
-    public ResponseEntity<Map<String, Object>> register(@RequestBody User user) {
-        Map<String, Object> result = userService.register(user.getUsername(), user.getPassword(), user.getUserEmail());
+    public ResponseEntity<Map<String, Object>> register(
+            @RequestParam String username, @RequestParam String password,@RequestParam(required = false) String userEmail) {
+        Map<String, Object> result = userService.register(username, password, userEmail);
         return ResponseEntity.status((Integer) result.get("code")).body(result);
     }
 
+    @Operation(summary="登录接口")
     @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody User user) {
-        Map<String, Object> result = userService.login(user.getUsername(), user.getPassword());
+    public ResponseEntity<Map<String, Object>> login(String username,String password) {
+        Map<String, Object> result = userService.login(username, password);
         return ResponseEntity.status((Integer) result.get("code")).body(result);
     }
 }
